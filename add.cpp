c8265e6c879a8a44e5cbcd87e6728044edf10e7b
embedcod <mulwaericbsc@gmail.com>
2024-09-13 17:26:42 +0300

    Initial commit

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int num1 = 5, num2 = 7, sum;
+
+    // Calculate the sum
+    sum = num1 + num2;
+
+    // Display the result
+    std::cout << "The sum of " << num1 << " and " << num2 << " is " << sum << "." << std::endl;
+
+    return 0;
+}
